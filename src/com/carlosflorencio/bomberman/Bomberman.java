3216a574c883e52c8b174589690887c773e76233
NicolasPL <nixter646464@gmail.com>
2022-11-26 00:23:52 -0500

    Adición de config.ini y excepciones básicas

--- src/com/carlosflorencio/bomberman/Bomberman.java
+++ src/com/carlosflorencio/bomberman/Bomberman.java
@@ -1,11 +1,79 @@
 package com.carlosflorencio.bomberman;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
 import com.carlosflorencio.bomberman.exceptions.BombermanException;
 import com.carlosflorencio.bomberman.gui.Frame;
 
 public class Bomberman {
-	
+	static ArrayList<Double> configOpciones = new ArrayList<Double>(6);
+	static int BOMBRATE_ = 1;
+	static int BOMBRADIUS_ = 1;
+	static double PLAYERSPEED_ = 1.0;
+	static int TIME_ = 200;
+	static int POINTS_ = 0;
+	static int LIVES_ = 3;
+
+	static public void leerConfig() {
+		boolean reintentar = false;
+
+		do {
+			String linea = new String();
+
+			File configFile = new File("config.ini");
+			try {
+				if (configFile.createNewFile() || reintentar) {
+					FileWriter defaultConfig = new FileWriter("config.ini");
+
+					defaultConfig.write("bomb_rate=1\nbomb_radius=1\nplayer_speed=1.0\ntime=200\npoints=0\nlives=3");
+					defaultConfig.close();
+					reintentar = false;
+				} else {
+
+					if (configFile.exists()) {
+						RandomAccessFile configFileRandom = new RandomAccessFile("config.ini", "r");
+
+						for (int i = 0; i < 6; i++) {
+							linea = configFileRandom.readLine();
+							StringTokenizer token = new StringTokenizer(linea, "=");
+							token.nextToken();
+
+							configOpciones.add(Double.valueOf(token.nextToken())); // Lo convierte a Double
+						}
+						configFileRandom.close();
+
+						BOMBRATE_ = configOpciones.get(0).intValue(); // Lo convierte a int
+						BOMBRADIUS_ = configOpciones.get(1).intValue(); // Lo convierte a int
+						PLAYERSPEED_ = (double) configOpciones.get(2);
+						TIME_ = configOpciones.get(3).intValue(); // Lo convierte a int
+						POINTS_ = configOpciones.get(4).intValue(); // Lo convierte a int
+						LIVES_ = configOpciones.get(5).intValue(); // Lo convierte a int
+
+						reintentar = false;
+					}
+				}
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			} catch (Exception e) {
+				System.out.println(
+						"Ha habido un problema con el archivo de configuración config.ini, creando uno nuevo...");
+				reintentar = true;
+			}
+
+		} while (reintentar);
+
+	}
+
 	public static void main(String[] args) throws BombermanException {
+		leerConfig();
 		Frame mainwindow = new Frame();
 	}
 }
--- src/com/carlosflorencio/bomberman/Game.java
+++ src/com/carlosflorencio/bomberman/Game.java
@@ -5,110 +5,106 @@ import java.awt.Graphics;
 import java.awt.image.BufferStrategy;
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferInt;
-
-
 import com.carlosflorencio.bomberman.exceptions.BombermanException;
 import com.carlosflorencio.bomberman.graphics.Screen;
 import com.carlosflorencio.bomberman.gui.Frame;
 import com.carlosflorencio.bomberman.input.Keyboard;
 
 public class Game extends Canvas {
-	
+
 	/*
 	|--------------------------------------------------------------------------
 	| Options & Configs
 	|--------------------------------------------------------------------------
 	 */
 	public static final double VERSION = 1.9;
-	
+
 	public static final int TILES_SIZE = 16,
-							WIDTH = TILES_SIZE * (int)(31 / 2), //minus one to ajust the window,
-							HEIGHT = 13 * TILES_SIZE;
+			WIDTH = TILES_SIZE * (int) (31 / 2), //minus one to ajust the window,
+			HEIGHT = 13 * TILES_SIZE;
 
 	public static int SCALE = 3;
-	
-	public static final String TITLE = "Bomberman " + VERSION;
-	
+
+	public static final String TITLE = "GOTY: The Game v" + VERSION;
+
 	//initial configs
-	private static final int BOMBRATE = 1;
-	private static final int BOMBRADIUS = 1;
-	private static final double PLAYERSPEED = 1.0;
-	
-	public static final int TIME = 200;
-	public static final int POINTS = 0;
-	public static final int LIVES = 3;
-	
+
+	private static final int BOMBRATE = Bomberman.BOMBRATE_;
+	private static final int BOMBRADIUS = Bomberman.BOMBRADIUS_;
+	private static final double PLAYERSPEED = Bomberman.PLAYERSPEED_;
+
+	public static final int TIME = Bomberman.TIME_;
+	public static final int POINTS = Bomberman.POINTS_;
+	public static final int LIVES = Bomberman.LIVES_;
+
 	protected static int SCREENDELAY = 3;
-	
-	
+
 	//can be modified with bonus
 	protected static int bombRate = BOMBRATE;
 	protected static int bombRadius = BOMBRADIUS;
 	protected static double playerSpeed = PLAYERSPEED;
-	
-	
+
 	//Time in the level screen in seconds
 	protected int _screenDelay = SCREENDELAY;
-	
+
 	private Keyboard _input;
 	private boolean _running = false;
 	private boolean _paused = true;
-	
+
 	private Board _board;
 	private Screen screen;
 	private Frame _frame;
-	
+
 	//this will be used to render the game, each render is a calculated image saved here
 	private BufferedImage image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
-	private int[] pixels = ((DataBufferInt)image.getRaster().getDataBuffer()).getData();
-	
+	private int[] pixels = ((DataBufferInt) image.getRaster().getDataBuffer()).getData();
+
 	public Game(Frame frame) throws BombermanException {
 		_frame = frame;
 		_frame.setTitle(TITLE);
-		
+
 		screen = new Screen(WIDTH, HEIGHT);
 		_input = new Keyboard();
-		
+
 		_board = new Board(this, _input, screen);
 		addKeyListener(_input);
 	}
-	
-	
+
 	private void renderGame() { //render will run the maximum times it can per second
 		BufferStrategy bs = getBufferStrategy(); //create a buffer to store images using canvas
-		if(bs == null) { //if canvas dont have a bufferstrategy, create it
+		if (bs == null) { //if canvas dont have a bufferstrategy, create it
 			createBufferStrategy(3); //triple buffer
 			return;
 		}
-		
+
 		screen.clear();
-		
+
 		_board.render(screen);
-		
+
 		for (int i = 0; i < pixels.length; i++) { //create the image to be rendered
 			pixels[i] = screen._pixels[i];
 		}
-		
+
 		Graphics g = bs.getDrawGraphics();
-		
+
 		g.drawImage(image, 0, 0, getWidth(), getHeight(), null);
 		_board.renderMessages(g);
-		
+
 		g.dispose(); //release resources
 		bs.show(); //make next buffer visible
 	}
-	
+
 	private void renderScreen() { //TODO: merge these render methods
 		BufferStrategy bs = getBufferStrategy();
-		if(bs == null) {
+		if (bs == null) {
 			createBufferStrategy(3);
 			return;
 		}
-		
+
 		screen.clear();
-		
+
 		Graphics g = bs.getDrawGraphics();
-		
+
 		_board.drawScreen(g);
 
 		g.dispose();
@@ -119,41 +115,40 @@ public class Game extends Canvas {
 		_input.update();
 		_board.update();
 	}
-	
+
 	public void start() {
 		_running = true;
-		
-		long  lastTime = System.nanoTime();
+
+		long lastTime = System.nanoTime();
 		long timer = System.currentTimeMillis();
 		final double ns = 1000000000.0 / 60.0; //nanosecond, 60 frames per second
 		double delta = 0;
 		int frames = 0;
 		int updates = 0;
 		requestFocus();
-		while(_running) {
+		while (_running) {
 			long now = System.nanoTime();
 			delta += (now - lastTime) / ns;
 			lastTime = now;
-			while(delta >= 1) {
+			while (delta >= 1) {
 				update();
 				updates++;
 				delta--;
 			}
-			
-			if(_paused) {
-				if(_screenDelay <= 0) { //time passed? lets reset status to show the game
+
+			if (_paused) {
+				if (_screenDelay <= 0) { //time passed? lets reset status to show the game
 					_board.setShow(-1);
 					_paused = false;
 				}
-					
+
 				renderScreen();
 			} else {
 				renderGame();
 			}
-				
-			
+
 			frames++;
-			if(System.currentTimeMillis() - timer > 1000) { //once per second
+			if (System.currentTimeMillis() - timer > 1000) { //once per second
 				_frame.setTime(_board.subtractTime());
 				_frame.setPoints(_board.getPoints());
 				_frame.setLives(_board.getLives());
@@ -161,13 +156,13 @@ public class Game extends Canvas {
 				_frame.setTitle(TITLE + " | " + updates + " rate, " + frames + " fps");
 				updates = 0;
 				frames = 0;
-				
-				if(_board.getShow() == 2)
+
+				if (_board.getShow() == 2)
 					--_screenDelay;
 			}
 		}
 	}
-	
+
 	/*
 	|--------------------------------------------------------------------------
 	| Getters & Setters
@@ -176,36 +171,36 @@ public class Game extends Canvas {
 	public static double getPlayerSpeed() {
 		return playerSpeed;
 	}
-	
+
 	public static int getBombRate() {
 		return bombRate;
 	}
-	
+
 	public static int getBombRadius() {
 		return bombRadius;
 	}
-	
+
 	public static void addPlayerSpeed(double i) {
 		playerSpeed += i;
 	}
-	
+
 	public static void addBombRadius(int i) {
 		bombRadius += i;
 	}
-	
+
 	public static void addBombRate(int i) {
 		bombRate += i;
 	}
-	
+
 	//screen delay
 	public int getScreenDelay() {
 		return _screenDelay;
 	}
-	
+
 	public void decreaseScreenDelay() {
 		_screenDelay--;
 	}
-	
+
 	public void resetScreenDelay() {
 		_screenDelay = SCREENDELAY;
 	}
@@ -213,30 +208,30 @@ public class Game extends Canvas {
 	public Keyboard getInput() {
 		return _input;
 	}
-	
+
 	public Board getBoard() {
 		return _board;
 	}
-	
+
 	public void run() {
 		_running = true;
 		_paused = false;
 	}
-	
+
 	public void stop() {
 		_running = false;
 	}
-	
+
 	public boolean isRunning() {
 		return _running;
 	}
-	
+
 	public boolean isPaused() {
 		return _paused;
 	}
-	
+
 	public void pause() {
 		_paused = true;
 	}
-	
+
 }
